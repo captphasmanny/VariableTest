156faf1851d7c6bf185645fc454701d960ef14c9
Manny <captphasmanny@att.net>
2023-11-06 09:20:11 -0500

    Initial Commit

---
+++ VariableTest/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  VariableTest
+//
+//  Created by Emanuel Alfaro on 11/6/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
